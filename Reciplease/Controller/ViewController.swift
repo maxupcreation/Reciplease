ef161f34aad1fe4dc000d2ea64b7adc7ed8990eb
max <>
2020-12-28 18:37:25 +0100

    Maxime
    First commit

---
+++ Reciplease/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Maxime on 28/12/2020.
+//  Copyright © 2020 Maxime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
